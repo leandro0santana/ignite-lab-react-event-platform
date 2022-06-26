087f0c7d7bac755a054466c233161d8a8c4677ad
Leandro C Santana <leandro.c.santana@hotmail.com>
2022-06-26 03:37:23 -0300

    Finalização do layout

---
+++ src/lib/apollo.ts
@@ -0,0 +1,7 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+export const client = new ApolloClient({
+  uri: import.meta.env.VITE_API_URL_GRAPHCMS,
+  headers: { 'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN_GRAPHCMS}` },
+  cache: new InMemoryCache()
+});
